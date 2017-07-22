063531699f48135796d07c06d73f60a6322180e1
Ian Huston <ian@ianhuston.net>
2017-07-22 12:12:15 +0100

    Added simple printing task

---
+++ 01-simple_print/printer.py
@@ -0,0 +1,7 @@
+import time
+
+print("Hello, I'm the task you requested.", flush=True) 
+time.sleep(5)
+print("Beep Boop Bing.", flush=True)
+time.sleep(5)
+print("Your task is now complete!", flush=True)
